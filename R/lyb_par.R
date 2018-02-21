2a9e0175c7f36e0d4e5ca812e198ccd76e70b2f6
admahood <admahood@gmail.com>
2018-02-21 15:01:01 +0000

    working

--- R/lyb_par.R
+++ R/lyb_par.R
@@ -5,19 +5,9 @@ library(doParallel)
 library(sp)
 library(sf)
 
-# Functions ----------------------------------------------------------------------------
-lyb_par <- function(splits){
-  
-  rcl <- raster::reclassify(splits,m)
-  return(rcl)
-  # saving just in case parallel only gets partially through
-}
-
 # Workflow _______________________________________________________________________________
 
-#setwd("~/fire_data_mongering")
 tif_path <- "data/" # path to a folder containing tif files
-#tif_path <- "~/DATA/FIRE/baecv/"
 result_path <- "results" # where you want the resulting rasters to go
 corz <- detectCores()
 
@@ -30,28 +20,32 @@ sp_grd <- sf::as_Spatial(grd)
 for(i in 1:length(tifs)){
   r <- raster(tifs[i])
   splits <- list()
-  
-  #cl <- makeCluster(corz)
+
   registerDoParallel(cores=corz) # for some reason this works better with doparallel and foreach
   splits <- foreach(j=1:length(sp_grd)) %dopar% raster::crop(r, sp_grd[j])
-  #stopCluster(cl)
-  
+
   rm(r)
+  
   year = as.integer(substr(splits[[1]]@data@names, 10,13)) # needs to be changed away from these magic numbers
   v = c(0,0,0,
         0.9,1.1,year)
   m = matrix(v, ncol=3, byrow=TRUE)
+  spl_rcl <- list()
   
   registerDoParallel(cores=corz)
-  spl_rcl <- foreach(k=1:length(splits)) %dopar% raster::reclassify(splits[[j]], m)
+  spl_rcl <- foreach(k=1:length(splits)) %dopar% raster::reclassify(splits[[k]], m)
   
-  # cl <- makeCluster(getOption("cl.cores", corz))
-  # spl_rcl <- parLapply(cl, c(splits, m), raster::reclassify)
-  # stopCluster(cl)
+  rm(splits)
   
   rcl_all <- do.call(raster::merge, spl_rcl)
-  writeRaster(rcl_all, file.path(result_path, paste0("lyb_",year, ".tif")))
-  rm(splits)
+  file <- file.path(result_path, paste0("lyb_",year, ".tif"))
+  writeRaster(rcl_all, file)
+  system(paste0("aws s3 cp ",
+                file, " ",
+                "s3://earthlab-ls-fire/lyb/lyb_",year,".tif"))
+  rm(rcl_all)
+  rm(spl_rcl)
+  
 }
 
 res_tifs <- Sys.glob(paste0(result_path,"lyb_*.tif"))
